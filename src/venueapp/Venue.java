0491647a3dbf0902e98acaea5b10667f07b6b6fc
Jayaraman <adarsh1999@gmail.com>
2018-09-23 21:00:21 +0530

    Added class Venue

--- src/venueapp/Venue.java
+++ src/venueapp/Venue.java
@@ -1,5 +1,59 @@
 package venueapp;
 
 public class Venue {
+	private String sname;
+	private String sphn;
+	private String vname;
+	private String location;
+	private String pictureURL;
+	private int costperhour;
+	public Venue(String sname, String sphn, String vname, String location,String pictureURL, int costperhour) {
+		super();
+		this.sname = sname;
+		this.sphn = sphn;
+		this.vname = vname;
+		this.location = location;
+		this.pictureURL = pictureURL;
+		this.costperhour = costperhour;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getSphn() {
+		return sphn;
+	}
+	public void setSphn(String sphn) {
+		this.sphn = sphn;
+	}
+	public String getVname() {
+		return vname;
+	}
+	public void setVname(String vname) {
+		this.vname = vname;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getPictureURL() {
+		return pictureURL;
+	}
+	public void setPictureURL(String pictureURL) {
+		this.pictureURL = pictureURL;
+	}
+	public int getCostperhour() {
+		return costperhour;
+	}
+	public void setCostperhour(int costperhour) {
+		this.costperhour = costperhour;
+	}
+
+	
+	
 
 }
